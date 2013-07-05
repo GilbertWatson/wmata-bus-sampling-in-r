d0c39c7a4d9aa28d0da871a969d8f6cd002a926f
Gilbert Watson <gwatson@gwatson-VirtualBox.(none)>
2013-07-05 13:47:43 -0400

    fixed error with file writing

--- GetDataDuringRushHours.r
+++ GetDataDuringRushHours.r
@@ -3,9 +3,9 @@ source("~/wmata-bus-sampling-in-r/MakeSample.r")
 ss <- 10
 LoadAPIToolsAndPopulationData()
 ToWriteToFile <- GetDataForSampleOfBusesNow(SampleSize=ss,SamplingFrame=getSamplingFrameNow())
-if (file.exists(paste0("~/wmata-bus-sampling-in-r/BusSample.",Sys.Date(),".csv"))) {
-  write.csv(ToWriteToFile,file=paste0("~/wmata-bus-sampling-in-r/sampledata/BusSample.",Sys.Date(),".csv"),append=T,row.names=F)
+if (file.exists(paste0("~/wmata-bus-sampling-in-r/sampledata/BusSample.",Sys.Date(),".csv"))) {
+  write.table(ToWriteToFile,file=paste0("~/wmata-bus-sampling-in-r/sampledata/BusSample.",Sys.Date(),".csv"),append=T,row.names=F,col.names=F,sep=",",eol="\n")
 }
-else {
-  write.csv(ToWriteToFile,file=paste0("~/wmata-bus-sampling-in-r/sampledata/BusSample.",Sys.Date(),".csv"),append=F,row.names=F)
+if (!file.exists(paste0("~/wmata-bus-sampling-in-r/sampledata/BusSample.",Sys.Date(),".csv"))) {
+  write.table(ToWriteToFile,file=paste0("~/wmata-bus-sampling-in-r/sampledata/BusSample.",Sys.Date(),".csv"),append=F,row.names=F,sep=",",eol="\n")
 }
