6b98c269da6c26f3878b20c5ed7e6c7409f3cf6d
Gilbert <gilbert@watsn.org>
2013-07-15 20:38:41 -0400

    be explicit

--- GetDataDuringRushHours.r
+++ GetDataDuringRushHours.r
@@ -3,7 +3,7 @@ source("~/wmata-bus-sampling-in-r/MakeSample.r")
 ss <- 30
 LoadAPIToolsAndPopulationData()
 #ToWriteToFile <- GetDataForSampleOfBusesNow(SampleSize=ss,SamplingFrame=getSamplingFrameNow(),stratify=T,option="Number.Of.Stops") #for stratified cluster sampling
-ToWriteToFile <- GetDataNow() #forsimple data creation with select routes "rt" defined in "MakeSample.r"
+ToWriteToFile <- GetDataNow(rout) #forsimple data creation with select routes "rt" defined in "MakeSample.r"
 if (file.exists(paste0("~/wmata-bus-sampling-in-r/sampledata/BusSample.",Sys.Date(),".csv"))) {
   write.table(ToWriteToFile,file=paste0("~/wmata-bus-sampling-in-r/sampledata/BusSample.",Sys.Date(),".csv"),append=T,row.names=F,col.names=F,sep=",",eol="\n")
 }
