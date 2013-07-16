5ea5b19ec150b6e384df336420df2eb1ebad094a
Gilbert <gilbert@watsn.org>
2013-07-15 20:36:14 -0400

    don't name things rt you idiot

--- MakeSample.r
+++ MakeSample.r
@@ -90,10 +90,10 @@ GetDataForSampleOfBusesNow <- function(SampleSize,SamplingFrame,stratify,option)
 }
 
 #get simple data
-rt <- c("90","X2","S1","D4","L2","64","D8","H1","S9","53",
+rout <- c("90","X2","S1","D4","L2","64","D8","H1","S9","53",
         "31","70","42","V7","P6","N4","D5","V9","16X","5A")
 
-GetDataNow <- function(buses=rt) {
+GetDataNow <- function(buses=rout) {
   BusesToSample <- buses
   SampleSize <- length(BusesToSample)
   BusSampleData <- NULL
